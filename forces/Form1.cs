e9310f815bfc9caf50703f667d0b40f31e7a53a2
Matthew Kenyon <rc0029540@student.riversidecollege.ac.uk>
2018-01-24 13:50:28 +0000

    Add project files.

---
+++ forces/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace forces
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double force = Convert.ToDouble(textBox1.Text);
+            double angle = Convert.ToDouble(textBox2.Text);
+            double fx = force * cos(angle);
+            double fy = force * sin(angle);
+            MessageBox.Show("fx =" + fx);
+            MessageBox.Show("fy =" + fy);
+        }
+
+        double sin(double x)
+        {
+            return (Math.Sin(x * Math.PI / 180.0));
+        }
+
+        double cos(double x)
+        {
+            return (Math.Cos(x * Math.PI / 180.0));
+        }
+
+        
+
+    }
+}
